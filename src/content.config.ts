8715589b8718d1243861b9cf442f038d0cc43a8f
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2025-07-16 10:51:13 -0400

    add astro collection for projects, skills, certifications, experiences and socials

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from "astro:content";
 
 // 2. Import loader(s)
 import { file } from "astro/loaders";
+import { date } from "astro:schema";
 
 // 3. Define your collection(s)
 
@@ -13,13 +14,14 @@ const projects = defineCollection({
     z.object({
       title: z.string(),
       description: z.string(),
-      image: image(),
-      date: z.date(),
+      image: image().optional(),
+      date: z.string().date(),
       tags: z.array(z.string()),
       body: z.string(),
       slug: z.string(),
       technologies: z.array(z.string()),
       results: z.array(z.string()),
+      link: z.string(),
     }),
 });
 
@@ -31,16 +33,50 @@ const skills = defineCollection({
       name: z.string(),
       yearsOfExperience: z.number().min(1).max(99),
       category: z.enum([
+        "Languages",
+        "Tools",
         "Backend Development",
         "Frontend Development",
         "Data Science",
-        "Computer Vision",
-        "System Design",
         "Databases",
       ]),
     }),
 });
 
+const certifications = defineCollection({
+  loader: file("src/data/certifications.json"),
+  schema: () =>
+    z.object({
+      slug: z.string(),
+      name: z.string(),
+      platform: z.string(),
+      date: z.string().date(),
+      link: z.string().url().optional(),
+    }),
+});
+
+const experiences = defineCollection({
+  loader: file("src/data/experiences.json"),
+  schema: () =>
+    z.object({
+      slug: z.string(),
+      name: z.string(),
+      yearsOfExperience: z.number().min(1).max(99),
+      link: z.string().url().optional(),
+    }),
+});
+
+
+const socials = defineCollection({
+  loader: file("src/data/socials.json"),
+  schema: () =>
+    z.object({
+      slug: z.string(),
+      type: z.enum(["github", "linkedin", "twitter", "instagram", "facebook"]),
+      name: z.string(),
+      link: z.string().url(),
+    }),
+});
 
 // 4. Export a single `collections` object to register your collection(s)
 export const collections = { skills, projects };
---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  en: "English",
+  es: "Spanish",
+};
+
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Acerca de",
+    "nav.twitter": "Twitter",
+  },
+} as const;
